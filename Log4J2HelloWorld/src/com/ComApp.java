a75f703494fca2144c137a4c03348b49b4f2e3c1
redab <redab@DESKTOP-V897U3L>
2020-05-12 02:59:24 +0200

    Version presque complete sans les filtres

--- Log4J2HelloWorld/src/com/ComApp.java
+++ Log4J2HelloWorld/src/com/ComApp.java
@@ -5,7 +5,15 @@ import org.apache.log4j.Logger;
 
 public class ComApp {
 	public ComApp(){
-		Logger logger = LogManager.getLogger(ComApp.class);			
+		Logger logger = LogManager.getLogger(ComApp.class);	
 		logger.trace("COM :: LEVEL :: ComApp TRACE Message ::");
+		
+//		Pour tester le RollingOver
+//		int i=0;
+//		while ( i<200000) {
+//			logger.trace("COM :: LEVEL :: ComApp TRACE Message ::");
+//			i++;
+//		}
+		
 		}
 }
